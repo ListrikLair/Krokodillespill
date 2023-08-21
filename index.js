60c7902c52c166157bea6506c07e899d7f10edb9
ListrikLair <141612577+ListrikLair@users.noreply.github.com>
2023-08-21 16:21:05 +0200

    Initial commit of krokodillespill
    Initial commit

---
+++ index.js
@@ -0,0 +1,63 @@
+var points = 0;
+var number1;
+var number2;
+var inputValue;
+
+randomizeNumbers();
+setPoints();
+
+function randomizeNumbers() {
+    number1 = Math.floor((Math.random() * 10) + 1);
+    number2 = Math.floor((Math.random() * 10) + 1);
+    document.getElementById("number1").innerHTML = number1
+    document.getElementById("number2").innerHTML = number2
+    document.getElementById("inputField").value = "";
+}
+
+function setPoints() {
+    document.getElementById("pointSum").innerHTML = points;
+}
+
+function guess() {
+    if (number1 == number2) {
+        if (inputField.value == "=") {
+            points++;
+            randomizeNumbers();
+            setPoints();
+        } else {
+            randomizeNumbers();
+            setPoints();
+        }
+    }
+    if (number1 < number2) {
+        if (inputField.value == "<") {
+            points++;
+            randomizeNumbers();
+            setPoints();
+        } else {
+            randomizeNumbers();
+            setPoints();
+        }
+    }
+    if (number1 > number2) {
+        if (inputField.value == ">") {
+            points++;
+            randomizeNumbers();
+            setPoints();
+        } else {
+            randomizeNumbers();
+            setPoints();
+        }
+    }
+}
+
+function reshuffle(){
+    randomizeNumbers()
+    setPoints();
+}
+
+function resetGame() {
+    points = 0;
+    randomizeNumbers()
+    setPoints();
+}
